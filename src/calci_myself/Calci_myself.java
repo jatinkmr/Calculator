d63b278c1de30b9deac91860fbb303db866de6d9
jatinkmr <kumar.jatin873@gmail.com>
2018-08-24 15:57:04 +0530

    first commit

---
+++ src/calci_myself/Calci_myself.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calci_myself;
+/**
+ *
+ * @author jatin kamboj
+ */
+public class Calci_myself {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
